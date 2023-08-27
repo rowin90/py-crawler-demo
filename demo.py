01a6db2d5b07de8d08003e9fc8b2fd101e056a22
饶驹 <raoju1580@qq.com>
2023-08-27 23:53:46 +0800

    feature(queue): 基于redis的zset实现优先级队列

--- demo.py
+++ demo.py
@@ -6,9 +6,20 @@
 # a = Res()
 # a.name = 3
 # print(a.name)
-import pickle
 
-a = pickle.dumps('a')
-print(a)
-b = pickle.loads(a)
-print(b)
+
+# import pickle
+#
+# a = pickle.dumps('a')
+# print(a)
+# b = pickle.loads(a)
+# print(b)
+
+
+from redis_queue.priority_redis_queue import PriorityRedisQueue
+
+q = PriorityRedisQueue('pqueue')
+q.put((100, 'value100'))
+
+r1 = q.get(block=False)
+print(r1)
