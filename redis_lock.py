5499ab4a17167fc90fda575156e0ad0e26713646
饶驹 <raoju1580@qq.com>
2023-08-28 22:50:14 +0800

    feature(lock): 锁的阻塞，自动获取线程id等

--- redis_lock.py
+++ redis_lock.py
@@ -1,24 +1,43 @@
+import time
+
 import redis
 import pickle
+
+
 class RedisLock(object):
-    def __init__(self,lock_name,host="localhost",port=6379,db=0):
+    def __init__(self, lock_name, host="localhost", port=6379, db=0):
         self.redis = redis.StrictRedis(host=host, port=port, db=db)
         self.lock_name = lock_name
 
-    def acquire_lock(self,thread_id):
+    def _get_thread_id(self):
+        import threading
+        import socket
+        import os
+        # thread_id = "服务器号" + "进程号" +  threading.currentThread().name
+        thread_id = socket.gethostname() + str(os.getpid()) + threading.currentThread().name
+        return thread_id
 
-        # 如果 lock_name 存在，ret == 0 否则 ret==1
-        ret = self.redis.setnx(self.lock_name,pickle.dumps(thread_id))
+    def acquire_lock(self, thread_id=None, expire=10 ,block = True):
 
-        if ret == 1:
-            self.redis.expire(self.lock_name,2)
-            print('上锁成功')
-            return True
-        else:
-            print('上锁失败')
-            return False
-    def release_lock(self,thread_id):
-        ret =  self.redis.get(self.lock_name)
+        if thread_id is None:
+            thread_id = self._get_thread_id()
+
+
+        while block:
+            # 如果 lock_name 存在，ret == 0 否则 ret==1
+            ret = self.redis.setnx(self.lock_name, pickle.dumps(thread_id))
+            if ret == 1:
+                self.redis.expire(self.lock_name, expire)
+                print('上锁成功')
+                return True
+            time.sleep(0.01)
+
+
+    def release_lock(self, thread_id=None):
+        if thread_id is None:
+            thread_id = self._get_thread_id()
+
+        ret = self.redis.get(self.lock_name)
 
         if ret is not None and pickle.loads(ret) == thread_id:
             self.redis.delete(self.lock_name)
@@ -28,12 +47,10 @@ class RedisLock(object):
             print('解锁失败')
             return False
 
+
 if __name__ == '__main__':
     redis_lock = RedisLock('redis_lock')
 
-    import threading
-    thread_id = "服务器号" + "进程号" +  threading.currentThread().name
-
-    if redis_lock.acquire_lock(thread_id):
+    if redis_lock.acquire_lock():
         print('执行对应的操作')
-        redis_lock.release_lock(thread_id)
+        redis_lock.release_lock()
